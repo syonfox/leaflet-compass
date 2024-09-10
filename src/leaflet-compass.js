796d29e0115bb5493f91ac87ed63c0ca28b7541a
Kier <syonfox@users.noreply.github.com>
2024-09-10 06:10:03 -0700

    Update leaflet-compass.js
    add center pos maybe

--- src/leaflet-compass.js
+++ src/leaflet-compass.js
@@ -12,6 +12,33 @@
         factory(window.L);
     }
 })(function (L) {
+//https://stackoverflow.com/questions/23762176/leaflet-custom-control-position-center
+    L.Map.include({
+        _initControlPos: function () {
+            var corners = this._controlCorners = {},
+                l = 'leaflet-',
+                container = this._controlContainer =
+                    L.DomUtil.create('div', l + 'control-container', this._container);
+
+            function createCorner(vSide, hSide) {
+                var className = l + vSide + ' ' + l + hSide;
+
+                corners[vSide + hSide] = L.DomUtil.create('div', className, container);
+            }
+
+            createCorner('top', 'left');
+            createCorner('top', 'right');
+            createCorner('bottom', 'left');
+            createCorner('bottom', 'right');
+
+            createCorner('top', 'center');
+            createCorner('middle', 'center');
+            createCorner('middle', 'left');
+            createCorner('middle', 'right');
+            createCorner('bottom', 'center');
+        }
+    });
+	
 
 L.Control.Compass = L.Control.extend({
 
